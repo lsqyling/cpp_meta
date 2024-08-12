86deadf46ba10de0d711ff82d862aaf5fe2a5e0d
lsqyling <1058018384@qq.com>
2024-08-13 01:35:44 +0800

    meta-alg

--- include/metann/facilities/type_list.hpp
+++ include/metann/facilities/type_list.hpp
@@ -4,7 +4,7 @@
 
 #ifndef CPP_META_INCLUDE_METANN_FACILITIES_TYPE_LIST_HPP
 #define CPP_META_INCLUDE_METANN_FACILITIES_TYPE_LIST_HPP
-#include <type_traits>
+#include <utility>
 #include "metann/metann_ns.hpp"
 METANN_NS_BEGIN
 template<size_t lhs, size_t rhs>
@@ -50,6 +50,13 @@ concept type_list = requires {
     typename TypeList::is_type_list;
     typename TypeList::type;
 };
+
+template<typename TypeValue>
+concept type_value = requires {
+    TypeValue::type;
+    TypeValue::value;
+};
+
 }
 
 template<concepts::type_list in>
@@ -93,6 +100,253 @@ struct last<type_list<H0, H1,Ts...>> : last<type_list<H1, Ts...>> {};
 
 template<concepts::type_list in>
 using last_t = last<in>::type;
+
+template<concepts::type_list in, typename out = type_list<>>
+struct drop_last;
+template<typename T, typename out>
+struct drop_last<type_list<T>, out>
+{
+    using type = out;
+};
+
+template<typename H0, typename H1, typename ...Ts, typename out>
+struct drop_last<type_list<H0, H1, Ts...>, out>
+        : drop_last<type_list<H1, Ts...>, typename out::template append<H0>> {};
+
+template<concepts::type_list in, typename out = type_list<>>
+using drop_last_t = drop_last<in, out>::type;
+
+//alg.
+template<concepts::type_list in, template<typename> typename F>
+struct map;
+
+template<template<typename> typename F, typename ...Ts>
+struct map<type_list<Ts...>, F> : type_list<typename F<Ts>::type...> {};
+
+template<concepts::type_list in, template<typename> typename F>
+using map_t = map<in, F>::type;
+
+template<concepts::type_list in, template<typename> class P, concepts::type_list out = type_list<>>
+struct filter : out {};
+
+template<template<typename> class P, concepts::type_list out, typename H, typename ...Hs>
+struct filter<type_list<H, Hs...>, P, out>
+        : std::conditional_t<
+                P<H>::value,
+                filter<type_list<Hs...>, P, typename out::template append<H>>,
+                filter<type_list<Hs...>, P, out>> {};
+
+template<concepts::type_list in, template<typename> class P>
+using filter_t = filter<in, P>::type;
+
+template<concepts::type_list in, typename E>
+struct remove
+{
+private:
+    template<typename F>
+    using is_not_E = is_not_same<F, E>;
+
+public:
+    using type = filter_t<in, is_not_E>;
+};
+
+template<concepts::type_list in, typename E>
+using remove_t = remove<in, E>::type;
+
+template<concepts::type_list in, template<typename> class P>
+struct remove_if
+{
+private:
+    template<typename E>
+    using not_P = typename negate<P>::template type<E>;
+public:
+    using type = filter_t<in, not_P>;
+};
+
+template<concepts::type_list in, template<typename> class P>
+using remove_if_t = remove_if<in, P>::type;
+
+template<typename T>
+struct id
+{
+    using type = T;
+};
+
+template<concepts::type_list in, typename init, template<typename, typename> class op>
+struct fold : id<init> {};
+
+template<typename acc, template<typename, typename> class op,
+        typename H, typename ...Hs>
+struct fold<type_list<H, Hs...>, acc, op>
+        : fold<type_list<Hs...>, typename op<acc, H>::type, op> {};
+
+template<concepts::type_list in, typename init, template<typename, typename> class op>
+using fold_t = fold<in, init, op>::type;
+
+
+template<concepts::type_list ...>
+struct concat;
+
+template<concepts::type_list in>
+struct concat<in> : in {};
+
+template<typename ...Ts0, typename ...Ts1>
+struct concat<type_list<Ts0...>, type_list<Ts1...>>
+        : type_list<Ts0..., Ts1...> {};
+
+template<concepts::type_list in0, concepts::type_list in1, concepts::type_list ...Ls>
+struct concat<in0, in1, Ls...> : concat<typename concat<in0, in1>::type, Ls...> {};
+
+template<concepts::type_list ...Ls>
+using concat_t = concat<Ls...>::type;
+
+template<concepts::type_list in, typename E>
+struct contain : std::false_type {};
+
+template<typename ...Ts, typename E>
+struct contain<type_list<Ts...>, E>
+        : std::bool_constant<(false || ... || std::is_same_v<Ts, E>)> {};
+
+template<concepts::type_list in, typename E>
+constexpr bool contain_v = contain<in, E>::value;
+
+constexpr size_t npos = -1ULL;
+
+namespace detail {
+template<concepts::type_list in, typename E, size_t offset>
+struct find_impl;
+
+template<typename E, size_t offset>
+struct find_impl<type_list<>, E, offset> : std::integral_constant<size_t, npos> {};
+
+template<typename E, typename H, typename ...Ts, size_t offset>
+struct find_impl<type_list<H, Ts...>, E, offset>
+        : std::conditional_t<
+                std::is_same_v<E, H>,
+                std::integral_constant<size_t, offset>,
+                find_impl<type_list<Ts...>, E, offset+1>> {};
+
+
+template<concepts::type_list in, template<typename> class P, size_t offset>
+struct find_impl_if;
+
+template<template<typename> class P, size_t offset>
+struct find_impl_if<type_list<>, P, offset> : std::integral_constant<size_t, npos> {};
+
+template<template<typename> class P,
+        typename H, typename ...Ts,
+        size_t offset>
+struct find_impl_if<type_list<H, Ts...>, P, offset>
+        : std::conditional_t<
+                P<H>::value,
+                std::integral_constant<size_t, offset>,
+                find_impl_if<type_list<Ts...>, P, offset + 1>> {};
+}
+
+template<concepts::type_list in, typename E>
+struct find : detail::find_impl<in, E, 0> {};
+
+template<concepts::type_list in, typename E>
+using find_t = find<in, E>::type;
+
+template<concepts::type_list in, typename E>
+constexpr size_t find_v = find<in, E>::value;
+
+template<concepts::type_list in, template<typename> class P>
+struct find_if : detail::find_impl_if<in, P, 0> {};
+
+template<concepts::type_list in, template<typename> class P>
+using find_if_t = find_if<in, P>::type;
+
+template<concepts::type_list in, template<typename> class P>
+constexpr size_t find_if_v = find_if<in, P>::value;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- test/metann/mpl_type_list.cpp
+++ test/metann/mpl_type_list.cpp
@@ -24,6 +24,7 @@ void test_aux()
     info(is_less);
     info(negate);
     info(is_not_same);
+    std::cout << "------------------\n";
 }
 
 template<typename ...Ts>
@@ -52,11 +53,116 @@ void test_type_list()
     info(head);
     info(tail);
     info(last);
+
+    using t4 = drop_last_t<list>;
+    static_assert(std::same_as<t4, type_list<char,int,long,float>>);
+    info(drop);
+    std::cout << "------------------\n";
+}
+
+template<size_t I>
+struct type_value
+{
+    using type = type_value;
+    static constexpr size_t value = I;
+};
+
+template<size_t I>
+using v = type_value<I>;
+
+template<typename T>
+struct double_
+{
+    using type = type_value<2*T::value>;
+};
+
+template<typename T>
+struct odd
+{
+    using type = T;
+    static constexpr bool value = T::value % 2 == 1;
+};
+
+template<typename U, typename T>
+struct add
+{
+    using type = v<U::value + T::value>;
+};
+
+template<typename T>
+struct bigger_eq
+{
+    static constexpr bool value = sizeof(T) >= 8;
+};
+
+
+void test_alg2()
+{
+    using t0 = type_list<char,int,long,float,double>;
+    static_assert(find_t<t0, char>::value == 0);
+    static_assert(find_v<t0, char> == 0);
+    static_assert(find_v<t0, int> == 1);
+    static_assert(find_v<t0, long> == 2);
+    static_assert(find_v<t0, float> == 3);
+    static_assert(find_v<t0, double> == 4);
+    static_assert(find_v<t0, short> == npos);
+    info(find);
+    static_assert(find_if_t<t0, bigger_eq>::value == 4);
+    static_assert(find_if_v<t0, bigger_eq> == 4);
+    info(find_if);
+
+
+
+    std::cout << "------------------\n";
 }
 
 
 
+void test_alg1()
+{
+    using t0 = type_list<char,int>;
+    using t1 = type_list<long>;
+    using t2 = type_list<float,double>;
 
+    using t3 = concat_t<t0>;
+    using t4 = concat_t<t0, t1, t2>;
+    static_assert(std::same_as<t3, t0>);
+    static_assert(std::same_as<t4, list>);
+    info(concat);
+    static_assert(contain_v<t4, double>);
+    info(contain);
+    std::cout << npos << std::endl;
+
+    std::cout << "------------------\n";
+}
+
+
+void test_alg()
+{
+    using t0 = type_list<v<0>, v<1>, v<2>, v<3>, v<4>, v<5>>;
+    using t1 = map_t<t0, double_>;
+    static_assert(std::same_as<t1, type_list<v<0>, v<2>, v<4>, v<6>, v<8>, v<10>>>);
+    info(map);
+    using t2 = filter_t<t0, odd>;
+    static_assert(std::same_as<t2, type_list<v<1>, v<3>, v<5>>>);
+    info(filter);
+
+    using t3 = type_list<char,int,float,double>;
+    using t4 = remove_t<t3, char>;
+    static_assert(std::same_as<t4, type_list<int,float,double>>);
+    using t5 = remove_t<t3, float>;
+    static_assert(std::same_as<t5, type_list<char,int,double>>);
+    info(remove);
+
+    using t6 = remove_if_t<t0, odd>;
+    static_assert(std::same_as<t6, type_list<v<0>,v<2>,v<4>>>);
+    info(remove_if);
+
+    using t7 = fold_t<t0, v<100>, add>;
+    static_assert(t7::value == 115);
+    info(fold);
+    std::cout << "------------------\n";
+}
 
 
 
@@ -79,6 +185,9 @@ void test_type_list()
 
 int main(int argc, char *argv[])
 {
+    test_alg2();
+    test_alg1();
+    test_alg();
     test_type_list();
     test_aux();
 
